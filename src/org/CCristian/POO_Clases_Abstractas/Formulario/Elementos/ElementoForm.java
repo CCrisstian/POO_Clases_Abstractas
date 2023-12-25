0fbba3ba5bf9c6dab26505a91ed11d617f4f70b9
CCrisstian <crisstiann.c@gmail.com>
2023-12-25 19:06:23 -0300

    1ra Clase Abstracta

--- src/org/CCristian/POO_Clases_Abstractas/Formulario/Elementos/ElementoForm.java
+++ src/org/CCristian/POO_Clases_Abstractas/Formulario/Elementos/ElementoForm.java
@@ -1,2 +1,32 @@
-package org.CCristian.POO_Clases_Abstractas.Formulario.Elementos;public class ElementoForm {
+package org.CCristian.POO_Clases_Abstractas.Formulario.Elementos;
+
+abstract public class ElementoForm {
+
+/*------------ATRIBUTOS----------------*/
+    protected String valor;
+    protected String nombre;
+/*------------ATRIBUTOS----------------*/
+
+
+/*------------SETTER----------------*/
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+/*------------SETTER----------------*/
+
+/*------------CONSTRUCTOR----------------*/
+    public ElementoForm() {
+    }
+
+    public ElementoForm(String nombre) {
+        this();
+        this.nombre = nombre;
+    }
+/*------------CONSTRUCTOR----------------*/
+
+
+/*------------MÉTODOS----------------*/
+    abstract public String DibujarHTML();
+/*------------MÉTODOS----------------*/
+
 }
